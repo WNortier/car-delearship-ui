d6659a9e7b275af70ac3b62fa5ff15a212d8540c
warwick <warwick.nortier@gmail.com>
2020-03-01 17:06:59 +0200

    successful get request

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,58 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    </div>
-
+    <!-- <div id="nav">
+      
+      
+    </div> -->
     <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">Auto Trader</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#" to="/">Home</b-nav-item>
+            <b-nav-item href="#" to="about">About</b-nav-item>
+          </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template v-slot:button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
-    <router-view/>
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
+    <!-- <router-link to="/">Home</router-link> 
+    <router-link to="/about">About</router-link> -->
   </div>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
 
 <style>
 #app {
@@ -53,10 +60,10 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
 }
 
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -67,5 +74,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,58 @@
 <template>
   <div class="about">
     <h1>This is an about page changed</h1>
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="12">
+          <b-table :busy="isBusy" refs="table" id="my-table" :items="myProvider">
+            <template v-slot:table-busy>
+              <div class="text-center text-danger my-2">
+                <b-spinner class="align-middle"></b-spinner>
+                <strong>Loading...</strong>
+              </div>
+            </template>
+          </b-table>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cars: [],
+      items: []
+    };
+  },
+  methods: {
+    myProvider: function myProvider(ctx) {
+      // this.isBusy = true
+
+      const promise = axios.get(
+        "https://api-tutor.herokuapp.com/v1/cars?page=" +
+          ctx.currentPage +
+          "&size=" +
+          ctx.perPage
+      );
+      // Must return a promise that resolves to an array of items
+      return promise.then(res => {
+        // Pluck the array of items off our axios response
+       
+        const items = res.data.splice(0, 10);
+        console.log(items);
+
+        // Must return an array of items or an empty array if an error occurred
+        return items || [];
+      });
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
+
+
