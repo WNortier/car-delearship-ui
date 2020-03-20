694007d9b42d94e7a8378bff32ecc68259b7364a
warwick <warwick.nortier@gmail.com>
2020-03-20 22:02:08 +0200

    added menu to statistics tab

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -4,14 +4,58 @@
     <button @click="fillData()">Randomize</button>
   </div>-->
 
-  <div class="container">
+  <div class="Statistics">
+    <section id="actions" class="py-4 mb-4 bg-light">
+      <b-container>
+        <b-row>
+          <div class="col-md-3 my-1">
+            <button class="btn btn-primary btn-block text-white" @click="getInventory()">
+              <i class="fas fa-plus"></i>
+              Total Inventory
+            </button>
+          </div>
+          <div class="col-md-3 my-1">
+            <b-dropdown
+              id="dropdown-1"
+              :text="colorDropDownText"
+              class="text-white"
+              block
+              variant="warning"
+            >
+              <b-dropdown-item
+                varient="dark"
+                v-for="(color, index) in colors"
+                :key="index"
+                :value="color"
+                @click="colorDropDownText = color; applyColorFilter()"
+              >{{color}}</b-dropdown-item>
+            </b-dropdown>
+          </div>
+          <div class="col-md-3 my-1">
+            <b-dropdown id="dropdown-1" :text="makeDropDownText" block variant="success">
+              <b-dropdown-item
+                varient
+                v-for="(make, index) in makes"
+                :key="index"
+                :value="make"
+                text-white
+                class="text-white"
+                @click="makeDropDownText = make; applyMakeFilter()"
+              >{{make}}</b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </b-row>
+      </b-container>
+    </section>
     <!-- <bar-chart :chartdata="chartdata" :options="options" /> -->
-
-    <bar-chart v-if="loaded" :chartdata="chartdata" :options="options" />
+    <b-container>
+      <bar-chart v-if="loaded" :chartdata="chartdata" :options="options" />
+    </b-container>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 import BarChart from "./BarChart.vue";
 import axios from "axios";
 export default {
@@ -19,32 +63,40 @@ export default {
     BarChart
   },
   data: () => ({
+    colorDropDownText: "Select Color",
+    makeDropDownText: "Select Make",
     loaded: false,
     chartdata: {
       labels: null,
-      datasets: [{
-        label: "Data One",
-        backgroundColor: [],
-        data: []
-      }]
+      datasets: [
+        {
+          label: "Data One",
+          backgroundColor: [],
+          data: []
+        }
+      ]
     },
     options: {
       responsive: true,
       maintainAspectRatio: false,
       scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
           }
-        }]
+        ]
       }
     }
   }),
   methods: {
-    generateColor: function () {
+    generateColor: function() {
       while (this.count > 0) {
-        this.chartdata.datasets[0].backgroundColor.push("#" + ((Math.random() * 0xffffff) << 0).toString(16))
-        this.count--
+        this.chartdata.datasets[0].backgroundColor.push(
+          "#" + ((Math.random() * 0xffffff) << 0).toString(16)
+        );
+        this.count--;
       }
     }
   },
@@ -53,21 +105,14 @@ export default {
     try {
       axios.get("https://api-tutor.herokuapp.com/v1/cars").then(r => {
         const response = r.data;
-        //console.log(response)
-        //var myArray = [];
-        var makes = response.map(function (el) {
+        var makes = response.map(function(el) {
           return el.make;
         });
-
-        var noDuplicateMakes = makes.filter(function (elem, index, self) {
-          return index === self.indexOf(elem);
-        });
-
-        var occurrences = {};
+        const occurrences = {};
         for (var i = 0, j = makes.length; i < j; i++) {
           occurrences[makes[i]] = (occurrences[makes[i]] || 0) + 1;
         }
-        this.count =  Object.keys(occurrences).length
+        this.count = Object.keys(occurrences).length;
         this.generateColor();
         this.chartdata.labels = Object.keys(occurrences);
         this.chartdata.datasets[0].label = Object.keys(occurrences);
@@ -77,61 +122,8 @@ export default {
     } catch (e) {
       console.error(e);
     }
-  }
-
-  // name: "LineChartContainer",
-  // components: { BarChart },
-  // data: () => ({
-  //   loaded: false,
-  //   chartdata: null,
-  //    options: {
-  //     responsive: true,
-  //     maintainAspectRatio: false
-  //   }
-  // }),
-  // async mounted() {
-  //   this.loaded = false;
-  //   try {
-  //     axios.get("https://api-tutor.herokuapp.com/v1/cars").then(r => {
-  //       console.log(r.data);
-  //       this.chartdata = r.data[0];
-  //       this.loaded = true;
-  //     });
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }
-
-  // ,
-  // data () {
-  //   return {
-  //     datacollection: null
-  //   }
-  // },
-  // mounted () {
-  //   this.fillData()
-  // },
-  // methods: {
-  //   fillData () {
-  //     this.datacollection = {
-  //       labels: [this.getRandomInt(), this.getRandomInt()],
-  //       datasets: [
-  //         {
-  //           label: 'Toyota',
-  //           backgroundColor: '#C34E35',
-  //           data: [this.getRandomInt(), this.getRandomInt()]
-  //         }, {
-  //           label: 'BMW',
-  //           backgroundColor: '#C3A735',
-  //           data: [this.getRandomInt(), this.getRandomInt()]
-  //         }
-  //       ]
-  //     }
-  //   },
-  //   getRandomInt () {
-  //     return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-  //   }
-  // }
+  },
+  computed: mapState(["inventory", "colors", "makes", "range"])
 };
 </script>
 
